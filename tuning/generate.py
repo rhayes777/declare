b8899c1de97342a5e34ca5343e8711af26114ff2
Richard <richardhayes777@gmail.com>
2022-12-24 17:55:43 +0000

    generate different style samples

--- tuning/generate.py
+++ tuning/generate.py
@@ -1,14 +1,21 @@
-import json
+#!/usr/bin/env python
 
+import argparse
+import json
 import pathlib
 
+parser = argparse.ArgumentParser()
 
-def stringify(dir_path):
+parser.add_argument("--test-directory", type=pathlib.Path, required=True)
+parser.add_argument("--source-directory", type=pathlib.Path, required=True)
+
+
+def stringify(directory: pathlib.Path) -> str:
     # Initialize an empty string to store the contents of the files
     contents = ""
 
     # Iterate over all the files and directories in the given directory
-    for path in pathlib.Path(dir_path).rglob('*.py'):
+    for path in directory.rglob('*.py'):
         # Read the contents of the file
         with open(path, 'r') as f:
             file_contents = f.read()
@@ -24,3 +31,6 @@ def add_example(test_directory, source_directory):
     completion = stringify(source_directory)
     with open("example.jsonl", "a+") as f:
         f.write(json.dumps({"prompt": prompt, "completion": completion}) + "\n")
+
+
+add_example(parser.parse_args().test_directory, parser.parse_args().source_directory)
