88004d627fe828a546a93ce2acb6a202656748f7
Richard <richardhayes777@gmail.com>
2022-12-24 17:42:56 +0000

    adapting to read in entire directory recursively

--- tuning/generate.py
+++ tuning/generate.py
@@ -1,14 +1,26 @@
 import json
 
-with open(
-        "example/test_example.py"
-) as f:
-    prompt = f.read()
+import pathlib
 
-with open(
-        "example/example.py"
-) as f:
-    completion = f.read()
 
-with open("example.jsonl", "a+") as f:
-    f.write(json.dumps({"prompt": prompt, "completion": completion}) + "\n")
+def stringify(dir_path):
+    # Initialize an empty string to store the contents of the files
+    contents = ""
+
+    # Iterate over all the files and directories in the given directory
+    for path in pathlib.Path(dir_path).rglob('*.py'):
+        # Read the contents of the file
+        with open(path, 'r') as f:
+            file_contents = f.read()
+
+        # Add a header line with the file path and the contents of the file to the overall contents string
+        contents += "> {}\n{}\n".format(path, file_contents)
+
+    return contents
+
+
+def add_example(test_directory, source_directory):
+    prompt = stringify(test_directory)
+    completion = stringify(source_directory)
+    with open("example.jsonl", "a+") as f:
+        f.write(json.dumps({"prompt": prompt, "completion": completion}) + "\n")
